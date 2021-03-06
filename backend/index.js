4010d23a94b75cd54718e24b95d07b9cc2b4dde5
fkwala <felicia.kwala@gmail.com>
2021-03-06 15:54:00 -0800

    install dependencies

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,33 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.use(express.urlencoded({ extended: false }))
+
+// Verification page for abused
+app.get('/verify', (req, res) => {
+	res.send('')
+});
+
+// Mainmenu for abused
 app.get("/", (req, res) => {
 	res.send("Hello World!");
+});
+
+// Mainmenu for abuser
+app.get("/error", (req, res) => {
+	res.send("Error!");
+});
+
+// Ordering system for women
+app.get("/order", (req, res) => {
+	res.send("Order pizza")
 });
 
 app.listen(port, () => {
 	console.log(`Listening at http://localhost:${port}`);
 });
+
+// Middleware function for Face recognition API
+function checkFace(req, res, next){
+	res.redirect('/');
+};
