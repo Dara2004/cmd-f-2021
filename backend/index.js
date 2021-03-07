e53d4a9b712bd4a9e3f5e49d20ba238fc0fb8992
fkwala <felicia.kwala@gmail.com>
2021-03-06 17:38:50 -0800

    Added twilio text

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
 const express = require("express");
 const app = express();
 const port = 5000;
 
 app.use(express.urlencoded({ extended: false }))
 
-// Verification page for abused
-app.get('/verify', (req, res) => {
-	res.send('')
-});
+// Twilio
+app.get('/', (req, res) => {
 
-// Mainmenu for abused
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+	// TEXT MESSAGE
+	var accountSid = 'AC92f2accb0d20fbc614a651bccff09ae4'; // Your Account SID from www.twilio.com/console
+	var authToken = process.env.TWILIO_AUTH;   // Your Auth Token from www.twilio.com/console
 
-// Mainmenu for abuser
-app.get("/error", (req, res) => {
-	res.send("Error!");
-});
+	var twilio = require('twilio');
+	var client = new twilio(accountSid, authToken);
 
-// Ordering system for women
-app.get("/order", (req, res) => {
-	res.send("Order pizza")
-});
+	client.messages.create({
+	body: 'Hello from Node post function',
+	to: '+17789291864',  // Text this number
+	from: '+17786523386' // From a valid Twilio number - Suppose this is 
+	})
+	.then((message) => res.send('The message was sent'))
+
+	// TEXT MESSAGE
+})
 
 app.listen(port, () => {
 	console.log(`Listening at http://localhost:${port}`);
 });
-
-// Middleware function for Face recognition API
-function checkFace(req, res, next){
-	res.redirect('/');
-};
