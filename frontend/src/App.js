a89710a965973e3c0b807f5617ef93b404e4961b
Dara Nguyen <dara.nguyenthuy@gmail.com>
2021-03-06 18:05:20 -0800

    added router (#3)
    * wip
    
    * wdsf
    
    * wip
    
    * added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import { Header } from './common/Header';
 import { Home } from './pages/Home';
+import { Landing } from './pages/Landing';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home/>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/landing">
+            <Landing />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
