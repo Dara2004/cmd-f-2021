90d12796d3f3d9150c041f239171d41d94041553
Dara Nguyen <duongnt1204@gmail.com>
2021-03-06 18:03:20 -0800

    added router

--- frontend/src/common/Header.js
+++ frontend/src/common/Header.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import appLogo from "../assets/appLogo.png"
 import "../styles/Header.css"
 
 export const Header = () => {
     return (
         <React.Fragment>
-        <div className="navBar">
-            <img src={appLogo} alt ="appLogo"/>
-            <h1 className="appTitle">Paz's Pizza</h1>
-            <div className="menuItem">menu</div>
-            <div className="menuItem">locations</div>
-            <div className="menuItem">about</div>
-            <div className="menuItem">login</div>
-        </div>
-        <hr style={{top: "160px", width: "971px"}}></hr>
+            <div className="navBar">
+                <img src={appLogo} alt ="appLogo"/>
+                <h1 className="appTitle">Paz's Pizza</h1>
+                <Link to="/menu"><div className="menuItem">menu</div></Link>
+                <Link to="/locations"><div className="menuItem">locations</div></Link>
+                <Link to="/about"><div className="menuItem">about</div></Link>
+                <Link to="/landing"><div className="menuItem">login</div></Link>
+            </div>
+            <hr style={{top: "160px", width: "971px"}}></hr>
         </React.Fragment>
     )
 }
